36110af531d1159db36134d44640f6d7376f59bd
Johnny Gérard <john@jgerard.dev>
2024-07-14 13:58:37 +0200

    feat: add contact form POST endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,48 @@
 import express from "express";
 import { env } from "node:process";
+import { MSGID_REGEX, transporter } from "./mail.js";
+import { validate } from "./form-validator.js";
+import { ContactForm } from "./types/contact-form.js";
+import { ContactFormQuery } from "./types/contact-form.enum.js";
 
 const PORT: number = +(env.PORT ?? 3000);
 const isProduction: boolean = env.NODE_ENV === "production";
 
 const app = express();
+const jsonParser = express.json();
 
 app.get("/", (_req, res) => {
   res.send("Hello, world!\n");
+});
+
+app.post("/contact-us", jsonParser, async (req, res, next) => {
+  try {
+    const isValid = validate(req.body);
+
+    if (!isValid) {
+      next(JSON.stringify(validate.errors));
+      return;
+    }
+
+    const form = req.body as ContactForm;
+    const info = await transporter.sendMail({
+      from: '"Mazie Breitenberg" <mazie3@ethereal.email>',
+      to: `"${form.firstName} ${form.lastName}" <${form.email}>`,
+      subject:
+        form.query === ContactFormQuery.GENERAL_INQUIRY
+          ? "General Inquiry"
+          : "Support Request",
+      text: form.message,
+    });
+
+    const msgId = info.response.match(MSGID_REGEX)?.groups?.msgId;
+    if (!msgId) throw Error("Failed to extract message ID");
+
+    const publicUrl = `https://ethereal.email/message/${msgId}`;
+    res.send(`Message sent! Public URL: ${publicUrl}\n`);
+  } catch (err) {
+    next(err);
+  }
 });
 
 if (isProduction) {
