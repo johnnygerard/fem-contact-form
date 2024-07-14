f85a462ba2b3a0f15c212ee7a49431a262bc0eb9
Johnny Gérard <john@jgerard.dev>
2024-07-14 06:55:35 +0200

    feat: add nodemailer config

---
+++ src/mail.ts
@@ -0,0 +1,17 @@
+import { env } from "node:process";
+import nodemailer from "nodemailer";
+
+export const MSGID_REGEX = /MSGID=(?<msgId>[a-zA-Z0-9-]+)/;
+
+if (!env.ETHEREAL_PWD) {
+  throw Error("Missing environment variable: ETHEREAL_PWD");
+}
+
+export const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  auth: {
+    user: "mazie3@ethereal.email",
+    pass: env.ETHEREAL_PWD,
+  },
+});
