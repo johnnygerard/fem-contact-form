259c31da345b38fdc0be513345c6e180a2f5ff72
Johnny Gérard <john@jgerard.dev>
2024-07-15 10:35:18 +0200

    feat: implement form core structure and validation

--- client/src/app/components/contact-form/contact-form.component.ts
+++ client/src/app/components/contact-form/contact-form.component.ts
@@ -1,17 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-contact-form',
+  selector: "app-contact-form",
   standalone: true,
-  imports: [],
-  templateUrl: './contact-form.component.html',
+  imports: [ReactiveFormsModule],
+  templateUrl: "./contact-form.component.html",
   styles: `
     :host {
       display: block;
     }
   `,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ContactFormComponent {
-
+  formControls = inject(FormBuilder).group({
+    firstName: ["", Validators.required],
+    lastName: ["", Validators.required],
+    email: ["", [Validators.required, Validators.email]],
+    query: ["", Validators.required],
+    message: ["", Validators.required],
+    consent: [false, Validators.requiredTrue],
+  });
 }
