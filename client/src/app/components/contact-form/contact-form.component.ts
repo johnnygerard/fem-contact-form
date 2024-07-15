deb115e004cf2716c1d7b32668c08252bd39387b
Johnny Gérard <john@jgerard.dev>
2024-07-15 12:03:17 +0200

    feat: add form validation messages

--- client/src/app/components/contact-form/contact-form.component.ts
+++ client/src/app/components/contact-form/contact-form.component.ts
@@ -1,5 +1,16 @@
-import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
-import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  viewChild,
+} from "@angular/core";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroupDirective,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
 
 @Component({
   selector: "app-contact-form",
@@ -14,6 +25,8 @@ import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ContactFormComponent {
+  ngForm = viewChild.required(FormGroupDirective);
+
   formControls = inject(FormBuilder).group({
     firstName: ["", Validators.required],
     lastName: ["", Validators.required],
@@ -22,4 +35,23 @@ export class ContactFormComponent {
     message: ["", Validators.required],
     consent: [false, Validators.requiredTrue],
   });
+
+  readonly REQUIRED_ERROR_MSG = "This field is required";
+
+  hasEmailError(): boolean {
+    const control = this.formControls.controls.email;
+    return control.hasError("email") && this.#shouldDisplayError(control);
+  }
+
+  hasRequiredError(control: FormControl): boolean {
+    return control.hasError("required") && this.#shouldDisplayError(control);
+  }
+
+  #shouldDisplayError(control: FormControl): boolean {
+    return control.touched || this.ngForm().submitted;
+  }
+
+  onSubmit(): void {
+    if (this.ngForm().invalid) return;
+  }
 }
