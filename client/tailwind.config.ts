7ba56a1ad1655089fd68d63cb234286fb80d84ea
Johnny Gérard <john@jgerard.dev>
2024-07-16 02:55:09 +0200

    chore: add color palette

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,5 +1,18 @@
 import type { Config } from "tailwindcss";
 
+const palette = {
+  gray: {
+    500: "#86A2A5",
+    900: "#2A4144",
+  },
+  green: {
+    200: "#E0F1E8",
+    600: "#0C7D69",
+  },
+  red: "#D73C3C",
+  white: "#FFF",
+};
+
 export default {
   content: ["./src/**/*.{html,ts}"],
   theme: {
